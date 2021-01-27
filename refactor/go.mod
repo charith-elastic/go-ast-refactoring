da95648d0cc3b84ff02119154917fda284e62b8a
Charith Ellawala <charith.ellawala@elastic.co>
2021-01-27 11:27:41 +0000

    Replace log package

--- refactor/go.mod
+++ refactor/go.mod
@@ -3,9 +3,16 @@ module github.com/charithe/go-ast-refactoring/refactor
 go 1.15
 
 require (
-	github.com/dave/dst v0.26.0
+	github.com/dave/dst v0.26.2
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/tools v0.0.0-20201011145850-ed2f50202694
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	github.com/elastic/cloud-on-k8s v0.0.0-20210127091202-2e743a563f20 // indirect
+	golang.org/x/mod v0.4.1 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	golang.org/x/tools v0.1.0
+	k8s.io/api v0.20.2 // indirect
+	k8s.io/apimachinery v0.20.2 // indirect
+	k8s.io/client-go v11.0.0+incompatible // indirect
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
 )
--- refactor/main.go
+++ refactor/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
-	"go/types"
 	"log"
 	"os"
 	"path/filepath"
@@ -14,13 +13,6 @@ import (
 	"github.com/dave/dst/decorator/resolver/goast"
 	"github.com/dave/dst/decorator/resolver/guess"
 	"golang.org/x/tools/go/packages"
-)
-
-const (
-	// ifaceNeedle is the name of the interface we are interested in refactoring.
-	ifaceNeedle = "Client"
-	// pkgNeedle is the name of the package where the interface is defined.
-	pkgNeedle = "github.com/elastic/cloud-on-k8s/pkg/utils/k8s"
 )
 
 func main() {
@@ -43,14 +35,9 @@ func main() {
 	pkgs, err := packages.Load(cfg, "./...")
 	exitOnErr(err)
 
-	// find the interface definition
-	iface, err := findInterface(pkgs)
-	if err != nil {
-		exitOnErr(err)
-	}
-
 	// create a new decorator with support for resolving imports
-	d := decorator.NewDecoratorWithImports(fset, "main", goast.New())
+	//d := decorator.NewDecoratorWithImports(fset, "main", goast.New())
+	d := decorator.NewDecoratorWithImports(fset, "main", goast.WithResolver(guess.WithMap(map[string]string{"github.com/elastic/cloud-on-k8s/pkg/utils/log": "ulog"})))
 
 	// rewrite sources
 	for _, pkg := range pkgs {
@@ -60,33 +47,7 @@ func main() {
 	}
 }
 
-func findInterface(pkgs []*packages.Package) (*types.Interface, error) {
-	for _, p := range pkgs {
-		if p.PkgPath == pkgNeedle {
-			return getInterface(p.Types, ifaceNeedle)
-		}
-
-		// look through package imports
-		for _, i := range p.Types.Imports() {
-			if i.Path() == pkgNeedle {
-				return getInterface(i, ifaceNeedle)
-			}
-		}
-	}
-
-	return nil, fmt.Errorf("failed to find %s.%s", pkgNeedle, ifaceNeedle)
-}
-
-func getInterface(p *types.Package, name string) (*types.Interface, error) {
-	obj := p.Scope().Lookup(name)
-	if obj != nil {
-		return obj.Type().Underlying().(*types.Interface), nil
-	}
-
-	return nil, fmt.Errorf("interface %s does not exist in %s", name, p.Path)
-}
-
-func rewrite(d *decorator.Decorator, pkg *packages.Package, f *ast.File, iface *types.Interface) {
+func rewrite(d *decorator.Decorator, pkg *packages.Package, f *ast.File) {
 	updated := false
 
 	df, err := d.DecorateFile(f)
@@ -98,40 +59,33 @@ func rewrite(d *decorator.Decorator, pkg *packages.Package, f *ast.File, iface *
 			return false
 		}
 
-		// is this a function call with at least a single argument?
-		fn, ok := node.(*dst.CallExpr)
-		if !ok || len(fn.Args) == 0 {
+		ve, ok := node.(*dst.ValueSpec)
+		if !ok || len(ve.Values) != 1 {
 			return true
 		}
 
-		// is it a selctor expression? (e.g. x.method())
-		se, ok := fn.Fun.(*dst.SelectorExpr)
+		ce, ok := ve.Values[0].(*dst.CallExpr)
 		if !ok {
 			return true
 		}
 
-		// find the type of the selection
-		selection, ok := pkg.TypesInfo.Selections[d.Ast.Nodes[se].(*ast.SelectorExpr)]
+		se, ok := ce.Fun.(*dst.SelectorExpr)
 		if !ok {
 			return true
 		}
 
-		// is the receiver the correct type?
-		if !isInterfaceFunc(iface, selection.Recv(), se.Sel.Name) {
+		//dst.Fprint(os.Stdout, se, nil)
+
+		ident, ok := se.X.(*dst.Ident)
+		if !ok {
 			return true
 		}
 
-		// get the first argument to the function
-		arg := d.Ast.Nodes[fn.Args[0].(dst.Expr)]
-		if arg == nil {
-			return true
-		}
-
-		// if the first argument is not a context, prepend a context
-		argType := pkg.TypesInfo.TypeOf(arg.(ast.Expr))
-		if argType != nil && argType.String() != "context.Context" {
-			log.Printf("Match found at %s", d.Fset.Position(d.Ast.Nodes[fn].Pos()))
-			fn.Args = append([]dst.Expr{&dst.CallExpr{Fun: &dst.Ident{Name: "Background", Path: "context"}}}, fn.Args...)
+		if ident.Path == "sigs.k8s.io/controller-runtime/pkg/log" && ident.Name == "Log" {
+			log.Printf("Match found at %s", d.Fset.Position(d.Ast.Nodes[se].Pos()))
+			newIdent := dst.NewIdent("Log")
+			newIdent.Path = "github.com/elastic/cloud-on-k8s/pkg/utils/log"
+			se.X = newIdent
 			updated = true
 		}
 
@@ -144,25 +98,10 @@ func rewrite(d *decorator.Decorator, pkg *packages.Package, f *ast.File, iface *
 	}
 }
 
-func isInterfaceFunc(iface *types.Interface, t types.Type, funcName string) bool {
-	if !types.Implements(t, iface) {
-		return false
-	}
-
-	for i := 0; i < iface.NumMethods(); i++ {
-		fn := iface.Method(i)
-		if fn.Name() == funcName {
-			return true
-		}
-	}
-
-	return false
-}
-
 func writeFile(fileName string, df *dst.File) {
 	log.Printf("Writing changes to %s", fileName)
 
-	restorer := decorator.NewRestorerWithImports("main", guess.New())
+	restorer := decorator.NewRestorerWithImports("main", guess.WithMap(map[string]string{"github.com/elastic/cloud-on-k8s/pkg/utils/log": "ulog"}))
 
 	out, err := os.Create(fileName)
 	if err != nil {
@@ -171,7 +110,10 @@ func writeFile(fileName string, df *dst.File) {
 
 	defer out.Close()
 
-	exitOnErr(restorer.Fprint(out, df))
+	fr := restorer.FileRestorer()
+	fr.Alias["github.com/elastic/cloud-on-k8s/pkg/utils/log"] = "ulog"
+
+	exitOnErr(fr.Fprint(out, df))
 }
 
 func exitOnErr(err error) {
